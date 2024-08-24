96c90cc8b6721e22e4049f89c1366526fe759999
Pavel <Pavel.Vlasov@nasdanika.org>
2024-08-24 12:13:01 -0400

    ActionSiteGenerator -> AppsiteGenerator

--- generator/src/test/java/org/nasdanika/models/pdf/generator/tests/TestPdfModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/pdf/generator/tests/TestPdfModelDocGen.java
@@ -23,7 +23,7 @@ import org.nasdanika.common.NullProgressMonitor;
 import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.html.bootstrap.Theme;
 import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.gen.ActionSiteGenerator;
+import org.nasdanika.html.model.app.gen.AppSiteGenerator;
 import org.nasdanika.models.ecore.graph.processors.EcoreActionGenerator;
 import org.nasdanika.models.ecore.graph.processors.EcoreNodeProcessorFactory;
 import org.nasdanika.models.pdf.PdfPackage;
@@ -76,7 +76,7 @@ public class TestPdfModelDocGen {
 		URI rootActionURI = URI.createFileURI(new File(rootActionResource).getAbsolutePath());//.appendFragment("/");
 		
 		String siteMapDomain = "https://pdf.models.nasdanika.org";		
-		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator() {
+		AppSiteGenerator actionSiteGenerator = new AppSiteGenerator() {
 			
 			protected boolean isDeleteOutputPath(String path) {
 				return !"CNAME".equals(path);				
