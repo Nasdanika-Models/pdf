3053d3a3cb7a0ece2e36d35e698635ca4535f4ff
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2024-11-14 05:56:34 -0500

    2024.11.0

--- model/src/main/java/org/nasdanika/models/pdf/util/PdfTextResourceFactoryCapabilityFactory.java
+++ model/src/main/java/org/nasdanika/models/pdf/util/PdfTextResourceFactoryCapabilityFactory.java
@@ -2,12 +2,17 @@ package org.nasdanika.models.pdf.util;
 
 import org.eclipse.emf.ecore.resource.Resource.Factory;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.nasdanika.capability.CapabilityFactory.Loader;
 import org.nasdanika.capability.emf.ResourceFactoryCapabilityFactory;
+import org.nasdanika.common.ProgressMonitor;
 
 public class PdfTextResourceFactoryCapabilityFactory extends ResourceFactoryCapabilityFactory {
 
 	@Override
-	protected Factory getResourceFactory(ResourceSet resourceSet) {
+	protected Factory getResourceFactory(
+			ResourceSet resourceSet, 
+			Loader loader,
+			ProgressMonitor progressMonitor) {
 		return new PdfTextResourceFactory();
 	}
 	
